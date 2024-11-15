6f88e773b0cb7946cf232f6401015beef0015f43
Firmansyah <firmansyah@unisayogya.ac.id>
2024-11-15 09:47:08 +0700

    add: table tahun ajaran

---
+++ database/migrations/2024_11_15_093910_create_tb_tahun_ajaran_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_tahun_ajaran', function (Blueprint $table) {
+            $table->bigIncrements('id_tahun_ajaran');
+            $table->string('nama_tahun_ajaran',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_tahun_ajaran');
+    }
+};
