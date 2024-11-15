0328366fb676e59dc532f1a08dd946cbb5cefaa8
Firmansyah <firmansyah@unisayogya.ac.id>
2024-11-15 10:12:27 +0700

    add: tabel capaian kompetensi

---
+++ database/migrations/2024_11_15_095532_create_tb_capaian_kompetensi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_capaian_kompetensi', function (Blueprint $table) {
+            $table->bigIncrements('id_capaian_kompetensi');
+            $table->bigInteger('id_mata_pelajran');
+            $table->bigInteger('id_kelas');
+            $table->text('target_capaian');
+            $table->foreign('id_mata_pelajaran')->references('id_mata_pelajaran')->on('tb_mata_pelajaran')->onDelete('cascade');
+            $table->foreign('id_kelas')->references('id_kelas')->on('tb_kelas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_capaian_kompetensi');
+    }
+};
