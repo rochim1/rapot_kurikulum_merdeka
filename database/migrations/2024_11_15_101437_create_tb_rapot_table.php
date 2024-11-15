44fd7e512997dde336a1a3e03ad6d8c30f11b303
Firmansyah <firmansyah@unisayogya.ac.id>
2024-11-15 10:39:45 +0700

    add: table rapot

---
+++ database/migrations/2024_11_15_101437_create_tb_rapot_table.php
@@ -0,0 +1,41 @@
+  <?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_rapot', function (Blueprint $table) {
+            $table->bigIncrements('id_rapot');
+            $table->bigInteger('id_siswa');
+            $table->bigInteger('id_kelas');
+            $table->enum('semester',['1','2']);
+            $table->integer('sakit');
+            $table->integer('izin');
+            $table->integer('tanpa_keterangan');
+            $table->text('catatan_wali_kelas');
+            $table->tinyInteger('ket_naik_kelas');
+            $table->string('nama_wali_kelas');
+            $table->bigInteger('nip_wali_kelas');
+            $table->string('nama_kepsek');
+            $table->bigInteger('nip_kepsek');
+            $table->foreign('id_siswa')->references('id_siswa')->on('tb_siswa')->onDelete();
+            $table->foreign('id_kelas')->references('id_kelas')->on('tb_kelas')->onDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_rapot');
+    }
+};
