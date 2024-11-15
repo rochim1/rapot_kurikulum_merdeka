0b025d43c1cacc878dd618dfa31b945887d643a3
Firmansyah <firmansyah@unisayogya.ac.id>
2024-11-15 11:09:28 +0700

    add: table profil sekolah

---
+++ database/migrations/2024_11_15_110457_create_tb_profil_sekolah_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_profil_sekolah', function (Blueprint $table) {
+            $table->bigIncrements('id_profil_sekolah');
+            $table->string('nama_sekolah');
+            $table->string('akreditas');
+            $table->string('npsn');
+            $table->string('alamat');
+            $table->string('kode_pos');
+            $table->string('telepon');
+            $table->string('email');
+            $table->string('nama_kepsek');
+            $table->string('nip_kepsek');
+            $table->string('pangkat_golongan_kepsek');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_profil_sekolah');
+    }
+};
