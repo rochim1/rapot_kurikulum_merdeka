933ff1fc19af7e1b89fa1acc6c3fbbc78f5feb60
rianpurnomo341 <76807575+rianpurnomo341@users.noreply.github.com>
2025-01-25 21:02:20 +0700

    capaian p5 baru select
    capaian p5 baru select

--- app/Http/Controllers/RapotP5CapaianProjekController.php
+++ app/Http/Controllers/RapotP5CapaianProjekController.php
@@ -2,18 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataProjekTargetCapaian;
+use App\Models\KelolaKelas;
+use App\Models\KelompokProjek;
+use App\Models\KelompokProjekDataProjek;
 use App\Models\RapotP5CapaianProjek;
+use App\Models\Siswa;
+use App\Models\TargetCapaian;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class RapotP5CapaianProjekController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $title = 'Input Nilai Projek';
+        $kelompokProjek = KelompokProjek::orderBy('nama')
+            ->where('id_tahun_ajaran', session('id_tahun_ajaran'))
+            ->where('id_kelas', session('id_kelas'))
+            ->where('id_user', auth()->user()->id)
+            ->get();
+    
+        if ($request->has('id_kelompok_projek')) {
+            $kelompokProjekDataProjek = KelompokProjekDataProjek::with('dataProjek')->where('id_kelompok_projek', $request->id_kelompok_projek)->get();
+            
+            if ($request->has('id_kelompok_projek_data_projek')) {
+                $id_data_projek = KelompokProjekDataProjek::where('id_kelompok_projek_data_projek', $request->id_kelompok_projek_data_projek)->first()->id_data_projek;
+                $dataProjekTargetCapaian = DataProjekTargetCapaian::with('targetCapaian')->where('id_data_projek', $id_data_projek)->get();
+
+                if ($request->has('id_target_capaian')) {
+                    $targetCapaian = TargetCapaian::find($request->id_target_capaian);
+
+                    $kelola_kelas = KelolaKelas::where('id_guru', auth()->user()->id)
+                                            ->where('id_kelas', session('id_kelas'))
+                                            ->where('id_tahun_ajaran', session('id_tahun_ajaran'))
+                                            ->get();
+
+                    $kelola_kelas->each(function ($kelola) {
+                        $kelola->siswa = Siswa::whereIn('id_siswa', $kelola->daftar_id_siswa)
+                                            ->where('status', 'active')
+                                            ->get();
+                                            
+                        $kelola->siswa->each(function ($siswa) use ($kelola) {
+                            $siswa->rapot = DB::table('tb_rapot')
+                                ->where('id_kelas', $kelola->id_kelas)
+                                ->where('id_tahun_ajaran', session('id_tahun_ajaran'))
+                                ->where('id_siswa', $siswa->id_siswa)
+                                ->first();
+                        });
+                    });
+
+
+                    return view('rapot_p5.nilai_capaian.create_edit_nilai_capaian', compact(
+                        'kelompokProjek',
+                        'kelompokProjekDataProjek',
+                        'dataProjekTargetCapaian',
+                        'targetCapaian',
+                        'kelola_kelas',
+                        'title',
+                    ));
+                }
+
+                return view('rapot_p5.nilai_capaian.pilih_dimensi', compact(
+                    'kelompokProjek',
+                    'kelompokProjekDataProjek',
+                    'dataProjekTargetCapaian',
+                    'title'
+                ));
+            }
+    
+            return view('rapot_p5.nilai_capaian.pilih_projek', compact(
+                'kelompokProjek',
+                'kelompokProjekDataProjek',
+                'title'
+            ));
+        }
+    
+        $title = 'Input Nilai Projek';
+        return view('rapot_p5.nilai_capaian.pilih_kelompok_projek', compact('title', 'kelompokProjek'));
+    } 
+    
 
     /**
      * Show the form for creating a new resource.
--- database/migrations/2025_01_24_074143_create_tb_rapot_p5_capaian_projeks_table.php
+++ database/migrations/2025_01_24_074143_create_tb_rapot_p5_capaian_projeks_table.php
@@ -12,16 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tb_rapot_p5_capaian_projek', function (Blueprint $table) {
-            $table->bigIncrements('id_rapot_p5_capaianprojek');
-            // $table->unsignedBigInteger('id_rapot')->nullable();
-            // $table->unsignedBigInteger('id_mata_pelajaran')->nullable();
-            // $table->integer('nilai_akhir')->nullable();
-            // $table->json('tujuan_pembelajaran_tercapai')->nullable();
-            // $table->json('tujuan_pembelajaran_tidak_tercapai')->nullable();
-            // $table->foreign('id_rapot')->references('id_rapot')->on('tb_rapot')->onDelete('cascade');
-            // $table->foreign('id_mata_pelajaran')->references('id_mata_pelajaran')->on('tb_mata_pelajaran')->onDelete('cascade');
-            // $table->timestamps();
-            // $table->softDeletes();
+            $table->bigIncrements('id_rapot_p5_capaian_projek');
+            $table->unsignedBigInteger('id_rapot')->nullable();
+            $table->unsignedBigInteger('id_kelompok_projek')->nullable();
+            $table->unsignedBigInteger('id_kel_pro_data_pro')->nullable();
+            $table->unsignedBigInteger('id_target_capaian')->nullable();
+            $table->string('predikat', 50)->nullable();
+
+            $table->foreign('id_rapot')->references('id_rapot')->on('tb_rapot')->onDelete('cascade');
+            $table->foreign('id_kelompok_projek')->references('id_kelompok_projek')->on('tb_kelompok_projek')->onDelete('cascade');
+            $table->foreign('id_kel_pro_data_pro')->references('id_kelompok_projek_data_projek')->on('tb_kelompok_projek_data_projek')->onDelete('cascade');
+            $table->foreign('id_target_capaian')->references('id_target_capaian')->on('tb_target_capaian')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
         });
     }
 
