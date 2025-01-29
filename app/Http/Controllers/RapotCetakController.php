a9f71a924a293cd6406421e1db995cc5850bc872
rochim1 <muhammad.0206@students.amikom.ac.id>
2025-01-29 11:28:32 +0700

    jadi fix

--- app/Http/Controllers/RapotCetakController.php
+++ app/Http/Controllers/RapotCetakController.php
@@ -10,6 +10,9 @@ use App\Models\TujuanPembelajaran;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
+use setasign\Fpdi\Fpdi;
+use setasign\Fpdi\PdfParser\StreamReader;
+
 class RapotCetakController extends Controller
 {
     /**
@@ -72,7 +75,7 @@ class RapotCetakController extends Controller
 
         $profilSekolah = ProfilSekolah::find(1);
         $nama_siswa = $rapot->first()->siswa->nama;
-        
+
         $pdf = Pdf::loadView('rapot_cetak.cetak.view_persiswa', compact('rapot', 'profilSekolah', 'nama_siswa'))
             ->setOptions(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true]);
 
@@ -82,6 +85,44 @@ class RapotCetakController extends Controller
             return $pdf->download('Rapot ' . $nama_siswa . '.pdf');
         }
     }
+
+    public function mergePdfs($rapot, $profilSekolah)
+    {
+        // Create a new FPDI instance to merge PDFs
+        $pdf = new Fpdi();
+
+        // Loop through the rapot collection
+        foreach ($rapot as $data) {
+            // Prepare the view data
+            $viewData = [
+                'rapot' => [0 => $data],
+                'profilSekolah' => $profilSekolah,
+                'nama_siswa' => $data->nama_siswa ?? '' // Access 'nama_siswa' as a model property
+            ];
+
+            // Generate the current PDF from the view
+            $pdfContent = Pdf::loadView('rapot_cetak.cetak.view_persiswa', $viewData)
+                ->setOptions(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true])
+                ->output(); // This will generate PDF content as a string
+
+            // Create a StreamReader instance from the generated PDF content
+            $streamReader = StreamReader::createByString($pdfContent);
+
+            // Set the source file to the PDF content in memory
+            $pageCount = $pdf->setSourceFile($streamReader);
+
+            // Add each page from the generated PDF to the FPDI instance
+            for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+                $pdf->AddPage();
+                $templateId = $pdf->importPage($pageNo);
+                $pdf->useTemplate($templateId);
+            }
+        }
+
+        // Output the merged PDF
+        return $pdf->Output('I', 'merged.pdf');
+    }
+
 
     public function rapot_all(Request $request)
     {
@@ -125,10 +166,11 @@ class RapotCetakController extends Controller
             }
             $profilSekolah = ProfilSekolah::find(1);
             $nama_siswa = 'Kelas ' . $rapot->first()->kelas->kelas_tingkatan . '.' . $rapot->first()->kelas->kelas_abjad;
+            return $this->mergePdfs($rapot, $profilSekolah);
 
-            $pdf = Pdf::loadView('rapot_cetak.cetak.view_persiswa', compact('rapot', 'profilSekolah', 'nama_siswa'))
-                ->setOptions(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true]);
-            return $pdf->stream('rapot kelas 1a.pdf');
+            // $pdf = Pdf::loadView('rapot_cetak.cetak.view_persiswa', compact('rapot', 'profilSekolah', 'nama_siswa'))
+            //     ->setOptions(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true]);
+            // return $pdf->stream('rapot kelas 1a.pdf');
         } else if ($request->download_all == 'rapot_p5') {
             $rapot = Rapot::with(['kelas', 'TahunAjaran', 'siswa', 'guru.user', 'RapotP5CapaianProjek.KelompokProjekDataProjek.dataProjek', 'RapotP5CatatanProsesProjek'])
                 ->where('id_tahun_ajaran', session('id_tahun_ajaran'))
